a01b7270a06497c6f4400b5f2238ac08f5934076
Kyle Krzeski <krzeski@vt.edu>
2016-07-24 21:47:08 -0400

    first commit

---
+++ app/scripts/app.js
@@ -0,0 +1,12 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name tirellamaApp
+ * @description
+ * # tirellamaApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('tirellamaApp', []);
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,30 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name tirellamaApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the tirellamaApp
+ */
+angular.module('tirellamaApp')
+  .controller('MainCtrl', ['$scope', function ($scope) {
+
+    $scope.years = [
+      '2016',
+      '2015',
+      '2014',
+      '2013',
+      '2012',
+      '2011',
+    ];
+
+    $scope.makes = [
+
+    ];
+
+    $scope.models = [
+
+    ];
+
+  }]);
